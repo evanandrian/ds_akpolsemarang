e269375b6103ccce422483f6fb58c87cd17eb2d0
evan andrian <efan.andrian05@gmail.com>
2023-03-20 09:25:15 +0700

    ds akpol

---
+++ DS_C.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+"C:\ds_akpolsemarang\kill_DS.exe" $1 &
+git pull
+"C:\ds_akpolsemarang\DesktopService.exe" $1 &
+
+
---
+++ DS_E.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+"E:\ds_akpolsemarang\kill_DS.exe" $1 &
+git pull
+"E:\ds_akpolsemarang\DesktopService.exe" $1 &
+
+
